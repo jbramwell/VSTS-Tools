71356b0b851c7bb5af2cfcd9b42419808f9be2a9
Jeff Bramwell <jeff@moonspace.net>
2016-04-17 15:37:46 -0500

    Added ListFiles and ListVariables tasks.

---
+++ VSTS-Extensions/VSTSToolsListFiles/listfiles.ps1
@@ -0,0 +1,23 @@
+param (
+    [string]$rootdir,
+    [string]$debugonly
+)
+
+Write-Verbose 'Entering listfiles.ps1'
+Write-Verbose "rootdir = $rootdir"
+Write-Verbose "debugonly = $debugonly"
+
+# Import the Task.Common dll that has all the cmdlets we need for Build
+import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
+
+if(!$rootdir)
+{
+    $rootdir = ".";
+}
+
+Write-Verbose "Setting root directory to $rootdir"
+
+if ($debugonly -eq "false" -or (($debugonly -eq "true") -and ($env:SYSTEM_DEBUG -eq "true")))
+{
+    dir $rootdir -r  | % { if ($_.PsIsContainer) { $_.FullName + "\" } else { $_.FullName } }
+}
---
+++ VSTS-Extensions/VSTSToolsListVariables/listvariables.ps1
@@ -0,0 +1,13 @@
+param (
+    [string]$debugonly
+)
+
+Write-Verbose 'Entering listvariables.ps1'
+
+# Import the Task.Common dll that has all the cmdlets we need for Build
+import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
+
+if ($debugonly -eq "false" -or (($debugonly -eq "true") -and ($env:SYSTEM_DEBUG -eq "true")))
+{
+    (Get-ChildItem Env:).GetEnumerator() | % { Write-Host ("{0} = {1}" -f $_.key, $_.value) }
+}
